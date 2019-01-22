c721da527c7b52a9587053a2ea0ef2aa51199771
Christopher <christopher.carnivale@gmail.com>
2019-01-22 12:47:36 -0500

    created QUESTIONS

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
-// Questions
+'use strict'; 
 
-// "Buy the ticket, take the ride."
-// "We are what we pretend to be, so we must be careful about what we pretend to be."
-// "I love deadlines.  I like the whooosing sound tehy make as they fly by."
-// "You can get all A's and still flunk life."
-// "Teh secret of getting ahead is getting started."
+const QUESTIONS = [
+  {
+    question: 'Buy the ticket, take the ride.',
+    correct: 'Hunter S. Thompson',
+    answers: ['Hunter S. Thompson', 'Joseph Heller', 'Ernest Hemingway', 'Henry David Thoreau']
+  },
+  {
+    question: 'We are what we pretend to be, so we must be careful about what we pretend to be.',
+    correct: 'Kurt Vonnegut',
+    answers: ['Ralph Waldo Emerson', 'H.G. Wells', 'Sir Arthur Conan Doyle', 'Kurt Vonnegut']
+  },
+  {
+    question: 'I love deadlines.  I like the whooosing sound they make as they fly by.',
+    correct: 'Douglas Adams',
+    answers: ['Philip Roth', 'J.K. Rowling', 'Douglas Adams', 'William Shakespeare']
+  },
+  {
+    question: 'You can get all A\'s and still flunk life.',
+    correct: 'Walker Percy', 
+    answers: ['Herman Melville', 'Walker Percy', 'Tom Wolfe', 'Fyodor Dostoevsky']
+  },
+  {
+    question: 'The secret of getting ahead is getting started.',
+    correct: 'Mark Twain',
+    answers: ['George Orwell', 'Mark Twain', 'Henry Miller', 'Ken Kesey']
+  } 
+]; 
 
-// Correct Answers
-// "Hunter S. Thompson"
-// "Kurt Vonnegut"
-// "Douglas Adams"
-// "Walker Percy"
-// "Mark Twain"
 
-// Other Choices
-// "Joseph Heller"
-// "Ernest Hemingway"
-// "Heny David Thoreau"
-// "Ralph Waldo Emerson"
-// "H.G. Wells"
-// "Sir Arthur Conan Doyle"
-// "Philip Roth"
-// "J.K. Rowling"
-// "William Shakespeare"
-// "Herman Melville"
-// "Tom Wolfe"
-// "Fyodor Dostoevsky"
-// "George Orwell"
-// "Henry Miller"
-// "Ken Kesey"
 
 // background image - http://www.horizon-advisors.com/wp-content/uploads/Books-on-blue.jpg
