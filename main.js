7acb64962805e6e97c05c7da2d69eb5d4a745651
Christopher <christopher.carnivale@gmail.com>
2019-01-22 17:37:04 -0500

    Everything but render function tested

--- main.js
+++ main.js
@@ -32,18 +32,23 @@ const STORE = {
   question: 0,
   correct: 0,
   view: 'start', 
-}
+};
 
 // question counter function
 function questionCounter() {
-    STORE.question++;
+  STORE.question++;
 }
 
 // ========== template generators ============
 
 // generate template for first/starting page
-function (){
-
+function generateStartPage(){
+  return `    
+  <div id='js-welcome'>
+    <h1>Welcome! Are you ready for our quiz?</h1>
+    <h2>This quiz is about ...</h2>
+    <button id='js-start-button'>Start Quiz</button>
+  </div>`;
 } 
 
 // generate template for questions
@@ -52,16 +57,16 @@ function generateQuestionsHTML() {
   return `<section id='js-questions-page'>
   <form>
       <fieldset>
-          <legend>Question ${currentQuestionCounter}</legend>
-              <input type='radio' checked>  ${currenQuestion.question}1<br>
-              <input type='radio'> potential answer 2<br>
-              <input type='radio'> potential answer 3<br>
-              <input type='radio'> potential answer 4<br>
+          <legend>Question ${currentQuestion.question}</legend>
+              <input type='radio' 'checked' ${currentQuestion.answers[0]}>  
+              <input type='radio' ${currentQuestion.answers[1]}>
+              <input type='radio  ${currentQuestion.answers[2]}>
+              <input type='radio' ${currentQuestion.answers[3]}>
       </fieldset> 
   </form>
 
   <button id='js-submit-button'>Submit</button>
-</section>`
+</section>`;
 }
 
 // generate template for user answering correctly
@@ -72,62 +77,62 @@ function generateCorrectResultsHTML() {
       <!-- maybe add link to happy picture/gif here -->
       <button id='js-next-button'>Next Question</button>
       <!-- progress/results -->
-      <span>So far you have ${score} / ${questionNumber}.</span>
-  </section>`
-  }
+      <span>So far you have ${STORE.correct} / ${STORE.question}.</span>
+  </section>`;
+}
 
 // generate template for user answering incorrectly
 function generateIncorrectResultsHTML() { 
   return `
     <section class='results-page'>
       <h2>Sorry! Wrong Answer.</h2>
-      <h4>The answer was ${correctAnswer}</h4>
+      <h4>The answer was ${QUESTIONS[STORE.question].correct}</h4>
       <!-- maybe add link to sad picture/gif here -->
       <button id='js-next-button'>Next Question</button>
       <!-- progress/results -->
-      <span>So far you have ${score} / ${questionNumber}.</span>
-    </section>`
+      <span>So far you have ${STORE.correct} / ${STORE.question}.</span>
+    </section>`;
 }
 
 
 // generate template for final page of results
 function generateFinalPageHTML() {
   return `<section class='final-page'>
-            <h2>You scored ${correctAnswerscounter} out of 10</h2>
+            <h2>You scored ${STORE.correct} out of 5</h2>
             <button id='js-reset-button'>Try Again?</button>
-          </section>`
+          </section>`;
 }
 
 
 function checkAnswer(userAnswer) {
-  if (userAnswer === QUESTIONS[i].correct) { 
+  if (userAnswer === QUESTIONS[STORE.question].correct) { 
     STORE.view = 'correct';
     STORE.correct++; 
   } 
   else {
-    STORE.view = 'incorrect' 
+    STORE.view = 'incorrect'; 
   }
 
 
-function render() {
-  if (STORE.view = 'start') {
-    $('form').html(generateStartPage());
-  } 
-  else if (STORE.view = 'final'){
-    $('form').html(generateFinalPageHTML()); 
-  }
-  else if (STORE.view = 'question'){
-      generateGenerateQuestionsHTML();
-  }
-  else if (STORE.view = 'correct')
-    generateCorrectResultsHTML(); 
-  }
-  else{
-    generateIncorrectResultsHTML(); 
+  function render() {
+    if (STORE.view === 'start') {
+      $('form').html(generateStartPage());
+    } 
+    else if (STORE.view === 'final'){
+      $('form').html(generateFinalPageHTML()); 
+    }
+    else if (STORE.view === 'question'){
+      generateQuestionsHTML();
+    }
+    else if (STORE.view === 'correct'){
+      generateCorrectResultsHTML(); 
+    }
+    else{
+      generateIncorrectResultsHTML(); 
+    }
   }
 
-
-
+}
 
 // ========== event handlers ============
 
